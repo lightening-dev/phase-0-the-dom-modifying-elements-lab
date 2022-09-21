41f6442a8eb0ba88ece239a8552d685b26cfc728
Lightening Gibson <lighteningibson@gmail.com>
2022-09-21 08:55:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const main = document.getElementById("main");
+
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+
+
+newHeader.id = "victory";
+
+
+
+
+newHeader.textContent = "Lighteing Gibson is the champion";
+document.body.append(newHeader);
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-describe("index.html", () => {
+describe("index.html", () => { 
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
       expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
